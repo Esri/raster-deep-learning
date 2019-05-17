aeeeb3f9eb08d05f6cb352390876366dd2ced263
Ling Tang <Ling_Tang@esri.com>
2019-05-16 17:22:22 -0700

    revision

--- env_setup.bat
+++ env_setup.bat
@@ -4,14 +4,14 @@ FOR /F "usebackq tokens=2*" %%A IN (`reg query "HKLM\Software\Microsoft\Windows\
 )
 SET SCRIPTS_DIR=%ARCGISPRO_INSTALL_DIR%bin\Python\Scripts\
 SET ENV_DIR=%ARCGISPRO_INSTALL_DIR%bin\Python\envs\
-SET ENV_NAME=DeepLearningEnvTest1
+SET ENV_NAME=DeepLearningEnvTest
 
 cd "%SCRIPTS_DIR%"
 CALL "%SCRIPTS_DIR%deactivate.bat"
 
 SET TF_PACKAGE=tensorflow-gpu
 SET CTNK_PACKAGE=cntk-gpu
-SET PYTORCH=https://download.pytorch.org/whl/cu100/torch-1.0.0-cp35-cp35m-win_amd64.whl
+SET PYTORCH=https://download.pytorch.org/whl/cu100/torch-1.0.0-cp36-cp36m-win_amd64.whl
 
 ECHO INFO: Clone a new ArcGIS python environment %ENV_NAME% ...
 CALL "%SCRIPTS_DIR%conda.exe" create --name %ENV_NAME% --clone arcgispro-py3
