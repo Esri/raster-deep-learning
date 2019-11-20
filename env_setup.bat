60254d9b7ee58c1bfab4160a321c0749f141d9ad
Ling Tang <Ling_Tang@esri.com>
2019-11-20 13:39:16 -0800

    updates to Pro 2.5

--- env_setup.bat
+++ env_setup.bat
@@ -11,21 +11,19 @@ CALL "%SCRIPTS_DIR%deactivate.bat"
 
 SET TF_PACKAGE=tensorflow-gpu
 SET CTNK_PACKAGE=cntk-gpu
-SET PYTORCH=https://download.pytorch.org/whl/cu100/torch-1.0.0-cp36-cp36m-win_amd64.whl
 
 ECHO INFO: Clone a new ArcGIS python environment %ENV_NAME% ...
 CALL "%SCRIPTS_DIR%conda.exe" create --name %ENV_NAME% --clone arcgispro-py3
 CALL "%SCRIPTS_DIR%activate.bat" %ENV_NAME%
 
 ECHO INFO: Install tensorflow-gpu, keras, and scikit-image ...
-CALL "%SCRIPTS_DIR%conda.exe" install -c anaconda %TF_PACKAGE%=1.13.1 keras=2.2.4 scikit-image=0.15.0 --yes
+CALL "%SCRIPTS_DIR%conda.exe" install -c anaconda %TF_PACKAGE%=1.14.0 keras=2.2.4 scikit-image=0.15.0 --yes
 
 ECHO INFO: Install cntk-gpu and Pillow ...
 CALL "%ENV_DIR%%ENV_NAME%\Scripts\pip.exe" install %CTNK_PACKAGE% Pillow==6.1.0
 
 ECHO INFO: Install Env for ArcGIS API For Python ...
-CALL "%SCRIPTS_DIR%conda.exe" install nb_conda_kernels=2.0.0 nb_conda=2.0.0 --yes
-CALL "%SCRIPTS_DIR%conda.exe" install -c fastai -c pytorch fastai=1.0.39 pytorch=1.0.0 torchvision=0.2.2 --yes
+CALL "%SCRIPTS_DIR%conda.exe" install -c fastai -c pytorch fastai=1.0.54 pytorch=1.1.0 torchvision=0.3.0 --yes
 
 ECHO INFO: Run proswap to switch to the new env ...
 CALL "%SCRIPTS_DIR%proswap.bat" %ENV_NAME%
